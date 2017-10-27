433f14fdcd7f4f29f625baa18027d88037a1e4af
Affan Sajid <affansajid@gmail.com>
2017-10-27 17:07:10 -0400

    Add NewDeck component placeholder

---
+++ components/NewDeck.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class NewDeck extends Component {
+  render() {
+    return (
+      <View>
+      <Text>NewDeck</Text>
+      </View>
+    )
+  }
+}
